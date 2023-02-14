dacf7b8d823537edbab3f94690d62f3325a836a1
Aung Pyae Sone <aungpyaesoneice@gmail.com>
2023-02-14 21:09:47 +0630

    text styling with annotate string builder

--- app/src/main/java/com/aps/compose_starter/MainActivity.kt
+++ app/src/main/java/com/aps/compose_starter/MainActivity.kt
@@ -23,7 +23,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,55 +41,23 @@ import com.aps.compose_starter.ui.theme.Compose_starterTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val painter = painterResource(id = R.drawable.kermit)
-            val title = "Smart style of the kermit"
-            val description = "Smart style of the kermit"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
-            }
 
-        }
     }
 }
 
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.width(200.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(painter, contentDescription, contentScale = ContentScale.Crop)
-            Box(modifier = Modifier.fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
-                )){}
-            
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) { Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp)) }
-        }
+/*setContent {
+    val painter = painterResource(id = R.drawable.kermit)
+    val title = "Smart style of the kermit"
+    val description = "Smart style of the kermit"
+    Box(modifier = Modifier
+        .fillMaxWidth(0.5f)
+        .padding(16.dp)) {
+        ImageCard(painter = painter, contentDescription = description, title = title)
     }
-}
+
+}*/
+
 
 @Composable
 fun MessageCard(msg: Message) {
